eef14218b346f54884050033a0e0dc1481b12a80
Sean Roddis <sean.roddis@check24.de>
2018-01-10 16:43:26 +0100

    added test project for DI

---
+++ BricksFx.Di.Ninject.Test/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BricksFx.Di.Ninject.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
