8dfa7b4d518f134e328dd46b18997c996b82a665
Sean Roddis <sean.roddis@check24.de>
2018-01-24 12:21:46 +0100

    added tests for lifetime

--- BricksFx.DI.Ninject.Test/NinjectContainerAdapter_when_register_dependencies.cs
+++ BricksFx.DI.Ninject.Test/NinjectContainerAdapter_when_register_dependencies.cs
@@ -24,6 +24,60 @@ namespace BricksFx.Di.Ninject.Test
         private NinjectContainerAdapter _adapter;
 
         [Test]
+        public void WHEN_lifetime_is_onRequest_THEN_instance_is_onRequest()
+        {
+            // Arrange
+            var dependencies = new IDependency[]
+                {new Dependency(typeof(ITestClass), typeof(TestClass), LifeTime.OnRequest)};
+
+            // Act
+            _adapter.Register(dependencies);
+
+            // Assert
+            var instance = _container.Get<ITestClass>();
+            instance.StringValue = "test";
+
+            var onRequest = _container.Get<ITestClass>();
+            onRequest.StringValue.Should().Be(null);
+        }
+
+        [Test]
+        public void WHEN_lifetime_is_singelton_THEN_instance_is_singelton()
+        {
+            // Arrange
+            var dependencies = new IDependency[]
+                {new Dependency(typeof(ITestClass), typeof(TestClass), LifeTime.Singleton)};
+
+            // Act
+            _adapter.Register(dependencies);
+
+            // Assert
+            var instance = _container.Get<ITestClass>();
+            instance.StringValue = "test";
+
+            var singelton = _container.Get<ITestClass>();
+            singelton.StringValue.Should().Be("test");
+        }
+
+        [Test]
+        public void WHEN_lifetime_is_transient_THEN_instance_is_transient()
+        {
+            // Arrange
+            var dependencies = new IDependency[]
+                {new Dependency(typeof(ITestClass), typeof(TestClass), LifeTime.Transient)};
+
+            // Act
+            _adapter.Register(dependencies);
+
+            // Assert
+            var instance = _container.Get<ITestClass>();
+            instance.StringValue = "test";
+
+            var transient = _container.Get<ITestClass>();
+            transient.StringValue.Should().Be(null);
+        }
+
+        [Test]
         public void WHEN_register_dependency_THEN_dependency_is_in_container()
         {
             // Arrange
--- BricksFx.DI.Ninject.Test/TestClasses/TestClass.cs
+++ BricksFx.DI.Ninject.Test/TestClasses/TestClass.cs
@@ -4,6 +4,6 @@ namespace BricksFx.Di.Ninject.Test.TestClasses
 {
     public class TestClass : ITestClass
     {
-        
+        public string StringValue { get; set; }
     }
 }
