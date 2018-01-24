31a4ac4dec40b86f34c3b4e4d269479ab0205369
Sean Roddis <sean.roddis@check24.de>
2018-01-24 12:33:41 +0100

    test configuration

--- post_build.cmd
+++ post_build.cmd
@@ -2,6 +2,13 @@ nuget install NUnit.Runners -Version 3.2.1 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.6.0 -OutputDirectory tools
  
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:.\tools\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe -targetargs:".\BricksFx.Core\bin\Release\BricksFx.Core.dll .\BricksFx.Core.Test\bin\Release\BricksFx.Core.Test.dll .\BricksFx.DI.Ninject.Test\bin\Release\BricksFx.DI.Ninject.Test.dll" -filter:"+[BricksFx.*]* -[*.Test]*" 
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe^
+ -register:user^
+ -target:.\tools\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe^
+ -targetargs:"^
+   .\BricksFx.Core\bin\Release\BricksFx.Core.dll^
+   .\BricksFx.Core.Test\bin\Release\BricksFx.Core.Test.dll^
+   .\BricksFx.DI.Ninject.Test\bin\Release\BricksFx.DI.Ninject.Test.dll"^
+ -filter:"+[BricksFx.Core]* +[*]* -[*.Test]*" 
  
 .\tools\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
