c8acabcafe83527a027487823f0fc5db80790670
sroddis <sean_roddis@hotmail.de>
2018-03-22 12:48:42 +0100

    renamed nuspec and changed pathes

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,12 +1,12 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\Mongo.Migration\bin\Release\Mongo.Migration.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile("$root\BricksFx\bin\Release\BricksFx.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\NuGet\Mongo.Migration.nuspec) 
+$content = (Get-Content $root\NuGet\BricksFx.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\nuget\Mongo.Migration.compiled.nuspec
+$content | Out-File $root\nuget\BricksFx.compiled.nuspec
 
-& $root\NuGet\NuGet.exe pack $root\nuget\Mongo.Migration.compiled.nuspec
+& $root\NuGet\NuGet.exe pack $root\nuget\BricksFx.compiled.nuspec
