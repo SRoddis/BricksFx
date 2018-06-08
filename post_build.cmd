78b86ce94e15dec8f591087053602b6527cce009
SRoddis <sean_roddis@hotmail.de>
2018-06-08 12:56:43 +0200

    added autofac to build and test

--- post_build.cmd
+++ post_build.cmd
@@ -5,7 +5,7 @@ nuget install coveralls.net -Version 0.6.0 -OutputDirectory tools
 .\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe ^
  -register:user ^
  -target:.\tools\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe ^
- -targetargs:".\BricksFx.Test\bin\Release\BricksFx.Test.dll .\BricksFx.Ninject.Test\bin\Release\BricksFx.Ninject.Test.dll" ^
+ -targetargs:".\BricksFx.Test\bin\Release\BricksFx.Test.dll .\BricksFx.Ninject.Test\bin\Release\BricksFx.Ninject.Test.dll .\BricksFx.Autofac.Test\bin\Release\BricksFx.Autofac.Test.dll" ^
  -filter:"+[BricksFx*]*" 
  
 .\tools\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
